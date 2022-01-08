41915d741de5272f0ac087653aa2393e4ffdf0b2
Mehul Tyagi <mehultyagi01@gmail.com>
2022-01-09 01:34:26 +0530

    CartScreen developing

--- client_app/src/App.js
+++ client_app/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Link, Switch } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import HomePage from "./screens/HomePage";
-import cartScreen from "./screens/cartScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Navbar />
       <BrowserRouter>
         <Route path="/" exact component={HomePage} />
-        <Route path="/cart" exact component={cartScreen} />
+        <Route path="/cart" exact component={CartScreen} />
       </BrowserRouter>
     </div>
   );
--- client_app/src/screens/cartScreen.js
+++ client_app/src/screens/cartScreen.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./cartScreen.css";
 
-function cartScreen() {
-  return <div>cartscreen</div>;
+function CartScreen() {
+  const cartstate = useSelector((state) => state.cartReducer);
+  const cartItems = cartstate.cartItems;
+  return (
+    <div className="cart flex justify-center">
+      <div className="cart_items flex-1 justify-center">
+        <h1 className="cart_heading">Cart</h1>
+        {cartItems.map((item) => {
+          return (
+            <div className="cart_item mb-3">
+              <h1>{item.name}</h1>
+              <p>
+                price: {item.quantity}*{item.price} = {item.price}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+      <div className="cart_cost flex-1 flex justify-center">
+        <h1>Cost</h1>
+      </div>
+    </div>
+  );
 }
 
-export default cartScreen;
+export default CartScreen;
