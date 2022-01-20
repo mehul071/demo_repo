a19af063c82088576a418403b8309be98d7294e7
Mehul Tyagi <mehultyagi01@gmail.com>
2022-01-20 19:51:19 +0530

    script changes

--- server.js
+++ server.js
@@ -14,12 +14,11 @@ app.use("/api/pizzas/", pizzaRoute);
 app.use("/api/users/", UserRoute);
 app.use("/api/orders/", placeRoute);
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client_app/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client_app/build/index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log("Server running on port 5000"));
